b4400caa2c0ae27771f57c4c3d6a63129001801b
Marcus Bieber <marcus.bieber@itspoon.com>
2019-09-06 10:03:05 +0200

    Started modules

---
+++ src/app/modules/division-module/division-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DivisionModuleRoutingModule } from './division-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DivisionModuleRoutingModule
+  ]
+})
+export class DivisionModuleModule { }
