30a28a82fdf92599fe86e86bc0f0f97f66052c18
Marcus Bieber <@arcuss-MBP.fritz.box>
2019-10-10 22:08:12 +0200

    uuid integration

--- src/app/modules/document/document-list/document-list.component.ts
+++ src/app/modules/document/document-list/document-list.component.ts
@@ -10,6 +10,11 @@ import { Router } from '@angular/router';
 import { takeWhile } from 'rxjs/operators';
 import { SearchService } from '@app/services/search.service';
 
+import uuidv1 from '@bundled-es-modules/uuid/v1.js';
+import uuidv3 from '@bundled-es-modules/uuid/v3.js';
+import uuidv4 from '@bundled-es-modules/uuid/v4.js';
+import uuidv5 from '@bundled-es-modules/uuid/v5.js';
+
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
@@ -46,6 +51,50 @@ export class DocumentListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log('ngOnInit: DocumentListComponent');
+
+    const v1options = {
+      node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
+      clockseq: 0x1234,
+      msecs: new Date(),
+      nsecs: 5678
+    };
+
+    const v4options = {
+      random: [
+        0x10,
+        0x91,
+        0x56,
+        0xbe,
+        0xc4,
+        0xfb,
+        0xc1,
+        0xea,
+        0x71,
+        0xb4,
+        0xef,
+        0xe1,
+        0x67,
+        0x1c,
+        0x58,
+        0x36
+      ]
+    };
+
+    /* const myId1 = uuid.v1();
+    console.log(myId1); */
+    const MY_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+
+    console.log(uuidv1());
+    // console.log(uuidv3());
+    console.log(uuidv4());
+    // console.log(uuidv5());
+
+    console.log('###########');
+
+    console.log(uuidv1(v1options));
+    console.log(uuidv3('HJi World', MY_NAMESPACE));
+    console.log(uuidv4(v4options));
+    console.log(uuidv5('hello world', MY_NAMESPACE));
 
     this.couchDBService
       .setStateUpdate()
