0ff01a314ee914a152ca505f71f01e3500a92416
Marcus Bieber <marcus.bieber@itspoon.com>
2019-11-12 13:45:48 +0100

    refactor pdf download action

--- src/app/modules/document/document-edit/document-edit.component.ts
+++ src/app/modules/document/document-edit/document-edit.component.ts
@@ -442,51 +442,7 @@ export class DocumentEditComponent implements OnInit, OnDestroy {
   }
 
   public getDownload(id: string, name: any) {
-    this.documentService
-      .getDownload(id, name)
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(
-        res => {
-          // It is necessary to create a new blob object with mime-type explicitly set
-          // otherwise only Chrome works like it should
-          const newBlob = new Blob([res], { type: 'application/pdf' });
-
-          // IE doesn't allow using a blob object directly as link href
-          // instead it is necessary to use msSaveOrOpenBlob
-          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveOrOpenBlob(newBlob);
-            return;
-          }
-
-          // For other browsers:
-          // Create a link pointing to the ObjectURL containing the blob.
-          const data = window.URL.createObjectURL(newBlob);
-
-          const link = document.createElement('a');
-          link.href = data;
-          link.download = name;
-          // this is necessary as link.click() does not work on the latest firefox
-          link.dispatchEvent(
-            new MouseEvent('click', {
-              bubbles: true,
-              cancelable: true,
-              view: window
-            })
-          );
-
-          setTimeout(() => {
-            // For Firefox it is necessary to delay revoking the ObjectURL
-            window.URL.revokeObjectURL(data);
-            link.remove();
-          }, 100);
-        },
-        error => {
-          console.log('download error:', JSON.stringify(error));
-        },
-        () => {
-          console.log('Completed file download.');
-        }
-      );
+    this.documentService.getDownload(id, name);
   }
 
   private getUsers(): void {
