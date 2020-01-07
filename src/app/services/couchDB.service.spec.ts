7806bce1b70cf81d231382e08e4bd48dfad76848
Marcus Bieber <marcus.bieber@itspoon.com>
2020-01-07 11:26:08 +0100

    update couchDBService test

--- src/app/services/couchDB.service.spec.ts
+++ src/app/services/couchDB.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { CouchDBService } from 'src/app/services/couchDB.service';
 import { Spy, createSpyFromClass } from 'jasmine-auto-spies';
 import { HttpClient } from '@angular/common/http';
@@ -7,6 +7,8 @@ import { Role } from '@app/models';
 describe('CouchDBService test', () => {
   let serviceUnderTest: CouchDBService;
   let httpSpy: Spy<HttpClient>;
+  let fakeObject: any;
+  let expectestFakeObject: any;
   let fakeRolesInput: {};
   let fakeRolesOutput: Role[];
   let actualResult: any;
@@ -22,9 +24,106 @@ describe('CouchDBService test', () => {
     serviceUnderTest = TestBed.get(CouchDBService);
     httpSpy = TestBed.get(HttpClient);
 
+    fakeObject = undefined;
+    expectestFakeObject = undefined;
     fakeRolesInput = undefined;
     fakeRolesOutput = undefined;
     actualResult = undefined;
+  });
+
+  describe('METHOD writeEntry', () => {
+    When(() => {
+      console.log(JSON.stringify(fakeObject));
+      serviceUnderTest
+        .writeEntry({})
+        .subscribe(value => (expectestFakeObject = value));
+    });
+
+    describe('GIVEN a successful request THEN return a object', () => {
+      Given(() => {
+        fakeObject = {
+          ok: true,
+          id: '1'
+        };
+
+        httpSpy.post.and.nextOneTimeWith(fakeObject);
+      });
+
+      Then(() => {
+        expect(expectestFakeObject).toEqual(fakeObject);
+      });
+    });
+  });
+
+  describe('METHOD updateEntry', () => {
+    When(() => {
+      serviceUnderTest.updateEntry(fakeObject, '1').subscribe(value => {
+        expectestFakeObject = value;
+      });
+    });
+
+    describe('GIVEN a successful request THEN return a object', () => {
+      Given(() => {
+        fakeObject = {
+          ok: true,
+          id: '1',
+          rev: '1'
+        };
+
+        httpSpy.put.and.nextOneTimeWith(fakeObject);
+      });
+
+      Then(() => {
+        expect(expectestFakeObject).toEqual(fakeObject);
+      });
+    });
+  });
+
+  describe('METHOD deleteEntry', () => {
+    When(() => {
+      serviceUnderTest.deleteEntry('1', '1').subscribe(value => {
+        expectestFakeObject = value;
+      });
+    });
+
+    describe('GIVEN a successful request THEN return a object', () => {
+      Given(() => {
+        fakeObject = {
+          _id: '1',
+          name: 'MyName'
+        };
+
+        httpSpy.delete.and.nextOneTimeWith(fakeObject);
+      });
+
+      Then(() => {
+        expect(expectestFakeObject).toEqual(fakeObject);
+      });
+    });
+  });
+
+  describe('METHOD fetchEntry', () => {
+    When(() => {
+      serviceUnderTest.fetchEntry('1').subscribe(value => {
+        expectestFakeObject = value;
+      });
+    });
+
+    describe('GIVEN a successful request THEN return a object', () => {
+      Given(() => {
+        fakeObject = {
+          _id: '1',
+          _rev: '1',
+          name: 'Customer name'
+        };
+
+        httpSpy.get.and.nextOneTimeWith(fakeObject);
+      });
+
+      Then(() => {
+        expect(expectestFakeObject).toEqual(fakeObject);
+      });
+    });
   });
 
   describe('METHOD: getRoles()', () => {
@@ -69,4 +168,27 @@ describe('CouchDBService test', () => {
       });
     });
   });
+  /* describe('METHOD: setStateUpdate', () => {
+    Given(() => {
+      // @ts-ignore
+      serviceUnderTest.updateSubject.next('Test2');
+      spyOn(serviceUnderTest, 'setStateUpdate');
+    });
+
+    When(() => {
+      // @ts-ignore
+      serviceUnderTest.setStateUpdate();
+    });
+
+    Then(
+      fakeAsync(() => {
+        // @ts-ignore
+        serviceUnderTest.setStateUpdate().subscribe(res => {
+          console.log('PP: ' + res);
+          expect(res).toEqual('Test2a');
+        });
+        tick(100000);
+      })
+    );
+  }); */
 });
