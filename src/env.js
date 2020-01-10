1100269c702fceb69803aafa37e948631a36782f
Marcus Bieber <marcus.bieber@itspoon.com>
2020-01-10 08:03:01 +0100

    Update search bar -> active search

--- src/env.js
+++ src/env.js
@@ -7,15 +7,15 @@
   window.__env.envFileLoaded = true;
 
   window.__env.dbIP = '127.0.0.1:5984';
-  //window.__env.dbBaseUrl = 'http://127.0.0.1:5984/';
+  window.__env.dbBaseUrl = 'http://127.0.0.1:5984/';
   // window.__env.dbName = 'norm_local';
-  //window.__env.dbName = 'norm_documents';
+  window.__env.dbName = 'norm_documents';
 
   // window.__env.dbBaseUrl = 'http://192.168.178.24:8888/';
   // window.__env.dbName = 'norm_rep';
 
-  window.__env.dbBaseUrl = 'http://116.203.220.19:5984/';
-  window.__env.dbName = 'norm_rep2';
+  // window.__env.dbBaseUrl = 'http://116.203.220.19:5984/';
+  // window.__env.dbName = 'norm_rep2';
 
   // window.__env.uploadUrl = 'http://116.203.220.19:4000/api/upload';
   window.__env.uploadUrl = 'http://localhost:4000/api/upload';
