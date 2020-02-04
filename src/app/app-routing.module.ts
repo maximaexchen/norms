9e4facf840d8644926b927b9218df8b757810b72
Marcus Bieber <marcus.bieber@itspoon.com>
2020-02-04 16:02:49 +0100

    Init new routing with external access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,63 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: RootComponent
+    component: RootComponent,
+    children: [
+      {
+        path: 'start',
+        component: StartComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'document',
+        // loadChildren: './modules/document/document.module#DocumentModule',
+        loadChildren: () =>
+          import('./modules/document/document.module').then(
+            m => m.DocumentModule
+          ),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'tag',
+        loadChildren: () =>
+          import('./modules/tag/tag.module').then(m => m.TagModule),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'group',
+        loadChildren: () =>
+          import('./modules/group/group.module').then(m => m.GroupModule),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('./modules/user/user.module').then(m => m.UserModule),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'role',
+        loadChildren: () =>
+          import('./modules/role/role.module').then(m => m.RoleModule),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('./modules/admin/admin.module').then(m => m.AdminModule),
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./modules/auth/auth.module').then(m => m.AuthModule)
+      },
+      {
+        path: 'logout',
+        loadChildren: () =>
+          import('./modules/auth/auth.module').then(m => m.AuthModule)
+      }
+    ]
   },
   {
     path: 'access-point',
@@ -17,54 +73,6 @@ const routes: Routes = [
         m => m.AccessPointModule
       ),
     canActivate: [AuthGuardService]
-  },
-  { path: 'start', component: StartComponent, canActivate: [AuthGuardService] },
-  {
-    path: 'document',
-    // loadChildren: './modules/document/document.module#DocumentModule',
-    loadChildren: () =>
-      import('./modules/document/document.module').then(m => m.DocumentModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'tag',
-    loadChildren: () =>
-      import('./modules/tag/tag.module').then(m => m.TagModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'group',
-    loadChildren: () =>
-      import('./modules/group/group.module').then(m => m.GroupModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'user',
-    loadChildren: () =>
-      import('./modules/user/user.module').then(m => m.UserModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'role',
-    loadChildren: () =>
-      import('./modules/role/role.module').then(m => m.RoleModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./modules/admin/admin.module').then(m => m.AdminModule),
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'logout',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
